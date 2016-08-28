871f5165e68e9bf46cdf2cc75d054e2cced6a926
dstovell <ninja@Daniels-iMac.local>
2016-08-28 03:16:07 -0700

    Character Demo

---
+++ CharacterDemo.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CharacterDemo : MonoBehaviour 
+{
+
+	public float DemoTime = 2f;
+	private float accumulatedTime = 0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Animator anim = this.gameObject.GetComponent<Animator>();
+		anim.SetBool("Run", true);
+		Screen.orientation = ScreenOrientation.Portrait;
+
+		#if UNITY_EDITOR
+		this.DemoTime = 2f;
+		#elif UNITY_IOS
+		this.DemoTime += 4f;
+		#endif
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.accumulatedTime += Time.deltaTime;
+		if (this.accumulatedTime > this.DemoTime)
+		{
+			SceneManager.LoadScene("GridPuzzleLoader");
+		}
+	}
+}
